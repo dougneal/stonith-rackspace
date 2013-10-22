cc7ef30638c2fac074fcabf4d57ab96a6ea0f6e1
Doug Neal <doug.neal@bjss.com>
2013-10-22 17:28:09 +0100

    Python rewrite

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -2,7 +2,9 @@
 
 rpm -U https://dl.fedoraproject.org/pub/epel/6Server/x86_64/epel-release-6-8.noarch.rpm
 yum -y groupinstall "Development tools"
-yum -y install libyaml tk
+yum -y install libyaml tk python-pip
 rpm -U http://repository.kestrel.bjsscloud.com/yum/ruby1.9-1.9.3.448-1.el6.x86_64.rpm
 gem1.9 install bundler
+pip install pyrax
+pip install pexpect
 
